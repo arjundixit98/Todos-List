7f6a0814e72816909444f466c70e52c55f882852
Arjun Dixit <you@example.com>
2021-10-13 14:28:53 +0530

    added name

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-            This is About component!
+            Create by Arjun Dixit
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error eaque natus neque rem architecto, sapiente sed, odio voluptas, sit odit alias accusantium! Laboriosam, in? Soluta sint corporis officiis ipsa delectus?</p>
         </div>
     )
